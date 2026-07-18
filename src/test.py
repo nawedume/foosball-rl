731419a8d0ff9b4e5b23d8a482b832274e845d00
Umar Nawed <umar.nawed@gmail.com>
2026-07-18 14:32:46 -0400

    Added intial base of the table in MuJoCo for use.
    Verified the placement visually through the MuJoCo viewer.
    
    Next steps: Add the articulated rods for the table and the ball.

--- src/test.py
+++ src/test.py
@@ -1,32 +1,35 @@
-# Throw a ball at 100 different velocities.
-
+import time
 import warp as wp
 
 wp.config.enable_mathdx_solver = False
 
 import mujoco
 import mujoco_warp as mjw
+import mujoco.viewer as m_viewer
 
-_MJCF=r"""
-<mujoco>
-  <worldbody>
-    <body>
-      <freejoint/>
-      <geom size=".15" mass="1" type="sphere"/>
-    </body>
-  </worldbody>
-</mujoco>
-"""
+with open("./model.xml") as f:
+    model_str = f.read()
 
+mjm = mujoco.MjModel.from_xml_string(model_str)
+mjd = mujoco.MjData(mjm)
 
-mjm = mujoco.MjModel.from_xml_string(_MJCF)
 m = mjw.put_model(mjm)
 d = mjw.make_data(mjm, nworld=100)
 
-# initialize velocities
-wp.copy(d.qvel, wp.array([[float(i) / 100, 0, 0, 0, 0, 0] for i in range(100)], dtype=float))
+dt = mjm.opt.timestep
 
-# simulate physics
-mjw.step(m, d)
+with m_viewer.launch_passive(mjm, mjd) as viewer:
 
-print(f'qpos:\n{d.qpos.numpy()}')
+    while viewer.is_running():
+        start = time.time()
+
+        mjw.step(m, d)
+        wp.synchronize()
+
+        mjw.get_data_into(result=mjd, mjm=mjm, d=d)
+
+        viewer.sync()
+
+        remaining = dt - (time.time() - start)
+        if remaining > 0:
+            time.sleep(remaining)
