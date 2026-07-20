ce5008fd27d8122ffb1b1319ca5e8f9b93d87c11
Umar Nawed <umar.nawed@gmail.com>
2026-07-19 20:44:40 -0400

    Added reset, reward and observation logic.
    We should now need only reset logic to complete the experiment setup.

---
+++ src/env.py
@@ -0,0 +1,210 @@
+from rsl_rl.env import VecEnv
+from dataclasses import dataclass
+from tensordict import TensorDict
+import torch
+import time
+import warp as wp
+# from typing import tuple
+
+wp.config.enable_mathdx_solver = False
+
+import mujoco
+import mujoco_warp as mjw
+import mujoco.viewer as m_viewer
+
+TERMINATION_HEIGHT = 0.15
+
+GOAL_CENTER_BLUE = [0.563284, 0.0, 0.289419]
+GOAL_CENTER_RED = [-0.550284, 0.0, 0.289419]
+
+class FoosballEnv(VecEnv):
+    """An environment for the foosball training."""
+
+    def __init__(self, num_envs: int = 1, dt: float = 1.0 / 60.0, device: str = "cuda:0", model="model.xml", sync_with_viewer=False) -> None:
+        super().__init__()
+
+        self.num_envs = num_envs
+        self.sync_with_viewer = sync_with_viewer
+
+        # for each 4 rods, we can rotate and slide, 4x2 = 8 actions.
+        # should output torques and forces for the motoros. See model.xml for definitions of actuators.
+        self.num_actions = 8
+
+        # 1 minute worth of steps
+        self.max_episode_length = int(60 / dt)
+        self.episode_length_buf = torch.zeros(num_envs, dtype=torch.long, device=device)
+
+        self.device = device
+        self.cfg = {}
+
+        wp.set_device(device)
+        # mujcoco init
+        with open("./model.xml") as f:
+            model_str = f.read()
+
+        self.mjm = mujoco.MjModel.from_xml_string(model_str)  # pyright: ignore[reportAttributeAccessIssue]
+        self.mjd = mujoco.MjData(self.mjm)  # pyright: ignore[reportAttributeAccessIssue]
+        self.mjm.opt.timestep = dt
+
+        # Might not need CPU buffer, can keep it on the GPU?
+        # self.mjd = mujoco.MjData(mjm)  # pyright: ignore[reportAttributeAccessIssue]
+
+        dt = self.mjm.opt.timestep
+
+        # on device data
+        self.model_d = mjw.put_model(self.mjm)
+        self.data_d = mjw.make_data(self.mjm, nworld=num_envs)
+
+        # Retrieve IDs for all items
+        self.ball_id = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_BODY, "ball"  # pyright: ignore[reportAttributeAccessIssue]
+        )
+
+        self.blue_rod_11 = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_BODY, "blue_11"  # pyright: ignore[reportAttributeAccessIssue]
+        )
+        self.blue_rod_12 = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_BODY, "blue_12"  # pyright: ignore[reportAttributeAccessIssue]
+        )
+        self.blue_rod_2 = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_BODY, "blue_2"  # pyright: ignore[reportAttributeAccessIssue]
+        )
+        self.blue_rod_3 = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_BODY, "blue_3"  # pyright: ignore[reportAttributeAccessIssue]
+        )
+
+        self.red_rod_11 = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_BODY, "red_11"  # pyright: ignore[reportAttributeAccessIssue]
+        )
+        self.red_rod_12 = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_BODY, "red_12"  # pyright: ignore[reportAttributeAccessIssue]
+        )
+        self.red_rod_2 = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_BODY, "red_2"  # pyright: ignore[reportAttributeAccessIssue]
+        )
+        self.red_rod_3 = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_BODY, "red_3"  # pyright: ignore[reportAttributeAccessIssue]
+        )
+
+
+        blue_goal_sensor_id = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_SENSOR,  # pyright: ignore[reportAttributeAccessIssue]
+            "blue_goal_reached"
+        )
+        self.blue_goal_sensor_adr = self.mjm.sensor_adr[blue_goal_sensor_id]
+
+        red_goal_sensor_id = mujoco.mj_name2id(  # pyright: ignore[reportAttributeAccessIssue]
+            self.mjm, mujoco.mjtObj.mjOBJ_SENSOR,  # pyright: ignore[reportAttributeAccessIssue]
+            "red_goal_reached"
+        )
+        self.red_goal_sensor_adr = self.mjm.sensor_adr[red_goal_sensor_id]
+
+        # unfortunately due to a bad table asset, this isn't symmetrical :(
+        # should update the asset later to be much better
+        self.blue_goal_center = torch.tensor(GOAL_CENTER_BLUE, device=self.device).unsqueeze(0)
+        self.red_goal_center = torch.tensor(GOAL_CENTER_RED, device=self.device).unsqueeze(0)
+
+        self.side = torch.zeros((self.num_envs), dtype=torch.int8)
+
+        self.goal_reward = 100.0
+
+
+    def get_observations(self) -> TensorDict:
+        # All this is pretty bad so we need to optimize. The idea here is that we want
+        # to ensure that the view for the policy is symmetrical regardless of what side its on.
+        # So we make the position relative to its own goal, and negate the velocity if its player 2 (red)
+        # We also ensure that the player state is first, and add a single bit of information stating the player
+        # so the policy can deal with asymmetry on the table, which exists because its a bad table asset.
+        # In the future, we should really optimize this, perhaps using Warp, and also use a better table asset.
+        q_pos = wp.to_torch(self.data_d.qpos).clone()
+        q_vel = wp.to_torch(self.data_d.qvel).clone()
+
+        # qpos and qvel should just be the cartesian coordinates
+        is_red = self.side == 1
+
+        ball_pos_rel = q_pos[:, 16:]
+        ball_pos_rel[:, :3] = torch.where(is_red, ball_pos_rel[:, 3] - self.red_goal_center, ball_pos_rel[:, 3] - self.blue_goal_center)
+
+        # reverse the direction so its always from the perspective of the current player
+        ball_vel_rel = q_vel[:, 16:]
+        ball_vel_rel[is_red, 0:2] = -ball_vel_rel[is_red, 0:2]
+
+        blue_pos = q_pos[:, :8]
+        blue_vel = q_vel[:, :8]
+        blue_state = torch.cat([blue_pos, blue_vel], dim=-1)
+
+        red_pos = q_pos[:, 8:16]
+        red_vel = q_vel[:, 8:16]
+        red_state = torch.cat([red_pos, red_vel], dim=-1)
+
+        player_rod_state = torch.where(is_red, red_state, blue_state)
+        op_rod_state = torch.where(is_red, blue_state, red_state)
+
+        data = torch.cat([player_rod_state, op_rod_state, ball_pos_rel, ball_vel_rel, self.side.to(torch.float).unsqueeze(1)], dim=1)
+
+        ret = { "policy": data }
+        return TensorDict(ret, batch_size=[self.num_envs], device=self.device)
+
+
+    def step(self, actions: torch.Tensor) -> tuple[TensorDict, torch.Tensor, torch.Tensor, dict]:
+        # TODO: use actions
+        mjw.step(self.model_d, self.data_d)
+
+        # Do I need this here if we just launch GPU kernels? Probably not remove later.
+        wp.synchronize()
+
+        self.episode_length_buf += 1
+
+        # used for termination and rewards
+        ball_pos = wp.to_torch(self.data_d.xpos)[:, self.ball_id]
+        out_of_bounds = ball_pos[:, 2] <= TERMINATION_HEIGHT
+
+        sensor_data = wp.to_torch(self.data_d.sensordata)
+        blue_goals = sensor_data[:, self.blue_goal_sensor_adr] > 0.5
+        red_goals = sensor_data[:, self.red_goal_sensor_adr] > 0.5
+
+        dones = (self.episode_length_buf > self.max_episode_length).bool() | out_of_bounds | blue_goals | red_goals
+        self.episode_length_buf[dones] = 0
+
+        # TODO: reset environments
+        env_reset_indices = dones.nonzero()
+        if env_reset_indices.shape[0] != 0:
+            self._reset(env_reset_indices)
+
+        obs = self.get_observations()
+
+        # include goal scoring here later.
+        in_goal = blue_goals | red_goals
+        goal_side = self.side[in_goal]
+
+        in_right_goal = ((goal_side == 0) & blue_goals) | ((goal_side == 1) & red_goals)
+        rewards = torch.zeros(self.num_envs,  device=self.device)
+        rewards[in_goal] = torch.where(in_right_goal, self.goal_reward, -self.goal_reward)
+
+        # add distance reward
+        # goal pivot is is (1 x 3), and the ball_pos is of sice (N, 3).
+        blue_side_not_in_goal = ~in_goal & (self.side == 0)
+        rewards[blue_side_not_in_goal] = 2.0 - torch.linalg.vector_norm(ball_pos[blue_side_not_in_goal, :] - self.red_goal_center, dim=1)
+
+        red_side_not_in_goal = ~in_goal & (self.side == 1)
+        rewards[red_side_not_in_goal] = 2.0 - torch.linalg.vector_norm(ball_pos[red_side_not_in_goal, :] - self.blue_goal_center, dim=1)
+
+        if self.sync_with_viewer:
+            self.get_sim_data()
+
+        return obs, rewards, dones, {}
+
+
+    def _reset(self, idx: torch.Tensor | None = None):
+        if idx is None:
+            ids = slice(None)
+            size = self.num_envs
+        else:
+            ids = idx
+            size = idx.shape[0]
+
+        self.side[ids] = torch.randint(0, 2, size=(size, ), dtype=torch.uint8)
+
+
+    def get_sim_data(self):
+        mjw.get_data_into(result=self.mjd, mjm=self.mjm, d=self.data_d)
--- src/test.py
+++ src/test.py
@@ -1,35 +1,25 @@
 import time
 import warp as wp
+import torch
 
 wp.config.enable_mathdx_solver = False
 
 import mujoco
 import mujoco_warp as mjw
 import mujoco.viewer as m_viewer
+from env import FoosballEnv
 
-with open("./model.xml") as f:
-    model_str = f.read()
+device = 'cpu'
+env = FoosballEnv(device=device, sync_with_viewer=True)
 
-mjm = mujoco.MjModel.from_xml_string(model_str)
-mjd = mujoco.MjData(mjm)
-
-m = mjw.put_model(mjm)
-d = mjw.make_data(mjm, nworld=100)
-
-dt = mjm.opt.timestep
-
-with m_viewer.launch_passive(mjm, mjd) as viewer:
-
+with m_viewer.launch_passive(env.mjm, env.mjd) as viewer:
     while viewer.is_running():
         start = time.time()
 
-        mjw.step(m, d)
-        wp.synchronize()
-
-        mjw.get_data_into(result=mjd, mjm=mjm, d=d)
+        env.step(torch.zeros(1, device=device))
 
         viewer.sync()
 
-        remaining = dt - (time.time() - start)
+        remaining = env.mjm.opt.timestep - (time.time() - start)
         if remaining > 0:
             time.sleep(remaining)
